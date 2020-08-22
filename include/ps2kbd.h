ccd89aa3842fb5c565adfb089ff86d57ec658ccc
Vitali Samurov <vitali.samurov@live.com>
2020-08-22 19:33:02 +0300

    LGPL v2.1 added in to ps2 sources

--- include/ps2kbd.h
+++ include/ps2kbd.h
@@ -1,32 +1,33 @@
+/*
+ps2kbd.h file is a part of stm32Basic project.
+
+Copyright (c) 2020 vitasam
+
+* Written by Christian Weichel <info@32leaves.net>.
+* Mostly rewritten Paul Stoffregen <paul@pjrc.com>, June 2010.
+* Modified for use with Arduino 13 by L. Abraham Smith, <n3bah@microcompdesign.com>. 
+* Modified for easy interrup pin assignement on method begin(datapin,irq_pin) by
+    Cuningan <cuninganreset@gmail.com>.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #ifndef _PS2KBD_H_
 #define _PS2KBD_H_
 
 #include <stdint.h>
 #include <stdbool.h>
-
-/*
-  PS2Keyboard.h - PS2Keyboard library
-  Copyright (c) 2007 Free Software Foundation.  All right reserved.
-  Written by Christian Weichel <info@32leaves.net>
-
-  ** Mostly rewritten Paul Stoffregen <paul@pjrc.com>, June 2010
-  ** Modified for use with Arduino 13 by L. Abraham Smith, <n3bah@microcompdesign.com> * 
-  ** Modified for easy interrup pin assignement on method begin(datapin,irq_pin). Cuningan <cuninganreset@gmail.com> **
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 
 /* 
  * Every call to read() returns a single byte for each
